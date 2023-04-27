66e9a1b182599bd49c58f5ed7e4c069d9ff4bcff
Lazaro Moreno <laz@thecaselygroup.com>
2023-04-27 12:58:58 -0400

    started codewars repo to save all my solved katas

---
+++ codewarsData/YouCantCodeUnderPressure1.ts
@@ -0,0 +1,10 @@
+// Solution
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// Other Solution Below
+
+// const doubleInteger = i => 2 * i;
+
+// Code as fast as you can! You need to double the integer and return it.
